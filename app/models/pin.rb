e33f5d46f2379b88103b1883670439cda084a402
Olivier de Roany <o_deroany@outlook.com>
2015-06-13 09:45:08 -0700

    changed pin.rb with s3 credentials

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,11 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, 
+	:styles => { :medium => "300x300>", :thumb => "100x100>" },
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
 end
